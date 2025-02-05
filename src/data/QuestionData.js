f0ec36ab5acf87ac19664c012fcd1c220ba30a0a
Mana013 <152575183+Mana013@users.noreply.github.com>
2025-02-05 15:43:37 +0700

    Web Quiz

---
+++ src/data/QuestionData.js
@@ -0,0 +1,30 @@
+const QuestionData = [
+    {
+        question:"ข้อใดไม่ใช่ tag html",
+        A:"<react>",
+        B:"<a>",
+        C:"<p>",
+        D:"<h1>",
+        answer:"A" 
+    },
+    {
+        question:"ข้อใดถูกต้องเกี่ยวกับ React",
+        A:"ทำงานผั่ง Server",
+        B:"ใช่จัดการฐาข้อมูล",
+        C:"เป็น JavaScrip Library",
+        D:"ถูกทุกข้อ",
+        answer:"C" 
+
+    },
+    {
+        question:"ข้อใดคือชื่อ Browser",
+        A:"PHP",
+        B:"React",
+        C:"MYSQL",
+        D:"Chrome",
+        answer:"D" 
+
+    }
+]
+
+export default QuestionData
